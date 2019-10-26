e03e3f63982a83b2190ca2f5a3323a62f3366fba
Mohammad Reza Mirzayee <shahrokh.mi@gmail.com>
2019-10-26 03:53:47 +0330

    changes to index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import express from ('express');
-import fs from ('fs');
-import geo from 'geolocation-utils'
+const express = require('express');
+const fs = require("fs")
+const geo = require('geolocation-utils')
 
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 var polygons = []
-
+const port = process.env.PORT || 3000;  
 app.listen(port , ()=> {
     let jsonData = JSON.parse(fs.readFileSync('sample-data.json'));
     let features =jsonData.features; 
@@ -52,5 +52,6 @@ app.put('/gis/addpolygon', (request,result) =>{
     
 });
 
-const port = process.env.PORT || 3000;
+
+
 
