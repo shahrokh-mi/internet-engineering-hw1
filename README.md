b64ba53203d2a530d694d0ff6459f7b8679e0d39
Mohammad Reza Mirzayee <shahrokh.mi@gmail.com>
2019-10-26 03:36:39 +0330

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# internet-engineering-hw1
-A simple node.js express application running two PUT and GET APIs to create plygons and finding if a given point is located in any of the stored polygons.
+heroku link : https://morning-sierra-21275.herokuapp.com/
+
+
+Artillery report:
+
+Started phase 0, duration: 1s @ 22:28:19(+0330) 2019-10-25
+Report @ 22:28:26(+0330) 2019-10-25
+Elapsed time: 8 seconds
+  Scenarios launched:  10
+  Scenarios completed: 10
+  Requests completed:  200
+  RPS sent: 28.9
+  Request latency:
+    min: 203.1
+    max: 1203.9
+    median: 218.7
+    p95: 756.3
+    p99: 1179.3
+  Codes:
+    200: 200
+
+All virtual users finished
+Summary report @ 22:28:26(+0330) 2019-10-25
+  Scenarios launched:  10
+  Scenarios completed: 10
+  Requests completed:  200
+  RPS sent: 28.86
+  Request latency:
+    min: 203.1
+    max: 1203.9
+    median: 218.7
+    p95: 756.3
+    p99: 1179.3
+  Scenario counts:
+    0: 10 (100%)
+  Codes:
+    200: 200
---
+++ index.js
@@ -0,0 +1,56 @@
+import express from ('express');
+import fs from ('fs');
+import geo from 'geolocation-utils'
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+var polygons = []
+
+app.listen(port , ()=> {
+    let jsonData = JSON.parse(fs.readFileSync('sample-data.json'));
+    let features =jsonData.features; 
+    features.forEach(element => {
+        let polygon = {
+            name : element.properties.name ,
+            coordinates : element.geometry.coordinates[0]
+        }
+
+        polygons.push(polygon);
+
+    });
+    
+    
+});
+
+app.get('/gis/testpoint' , (request,result)=>{
+    
+    location = [parseFloat(request.query.lat) , parseFloat(request.query.lng)];
+    resaults = [];
+    polygons.forEach(item => {
+        if (geo.insidePolygon(location,item.coordinates)){
+            resaults.push(item.name);
+        }
+    });
+
+    result.send({polygons : resaults});
+
+})
+
+app.put('/gis/addpolygon', (request,result) =>{
+    data = request.body;
+
+    let polygon = {
+        name : data.properties.name ,
+        coordinates : data.geometry.coordinates[0]
+    }
+
+    polygons.push(polygon);
+
+    result.send({status : "success"});
+    
+});
+
+const port = process.env.PORT || 3000;
+
